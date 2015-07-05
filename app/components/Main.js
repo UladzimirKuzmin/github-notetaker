3eddf067081f7e4b76b08ad802498d764203e68b
Vladimir.Kuzmin <specialkv@gmail.com>
2015-07-05 10:50:50 +0300

    adding first react code

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+
+var Main = React.createClass({
+	render: function() {
+		return (
+			<div>
+				Hello World
+			</div>			
+		)
+	}
+});
+
+React.render(<Main />, document.getElementById('app'));
