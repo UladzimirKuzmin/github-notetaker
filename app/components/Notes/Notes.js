39e9e650b80da430dca3c98486fb50e2c6a32630
Vladimir.Kuzmin <specialkv@gmail.com>
2015-07-05 16:23:31 +0300

    adding firebase ref to app

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var NotesList = require('./NotesList');
 
 var Notes = React.createClass({
 	propTypes: {
@@ -8,9 +9,8 @@ var Notes = React.createClass({
 	render: function() {
 		return (
 			<div>
-				Notes: <br />
-				Username: {this.props.username}<br />
-				Notes: {this.props.notes}
+				<h3>Notes fot {this.props.username}</h3>
+				<NotesList notes={this.props.notes} />
 			</div>		
 		)
 	}
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,17 +3,27 @@ var Router = require('react-router');
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
-	mixins: [Router.State],
+	mixins: [Router.State, ReactFireMixin],
 	getInitialState: function() {
 		return {
-			notes: ['note1', 'note2'],
+			notes: [],
 			bio: {
 				name: 'Tyler'
 			},
 			repos: ['repo1', 'repo2']
 		};
+	},
+	componentDidMount: function() {
+		this.ref = new Firebase('https://github-note-taker.firebaseio.com');
+		var childRef = this.ref.child(this.getParams().username);
+		this.bindAsArray(childRef, 'notes');
+	},
+	componentWillUnmount: function() {
+		this.unbind('notes');		
 	},
 	render:function() {
 		var username = this.getParams().username;
